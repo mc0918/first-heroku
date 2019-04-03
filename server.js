386de54b3b02ad85e2735c7e0d217a38de93668a
mc0918 <mcampbell0918@gmail.com>
2019-04-03 12:43:49 -0500

    created basic app

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+//app.listen always goes at the end of your code
+app.listen(PORT, function() {
+  console.log("App listening on PORT:" + PORT);
+});
